60c6f48ae11506ebdea176e0520b944178231a35
wangyoupeng <wangyoupeng@oppo.com>
2021-07-12 09:52:31 +0800

    对Activity的生命周期进行理解,使用logcat输出各种回调方法的执行

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -3,17 +3,20 @@ package com.example.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public static final String TAG_MYAPP = "MyFirstApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG_MYAPP, "onCreate method runs!");
     }
 
     public void sendMessage(View view) {
@@ -22,5 +25,48 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG_MYAPP, "onStart method runs!!");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG_MYAPP, "onRestart method runs!!");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG_MYAPP, "onResume method runs!!");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG_MYAPP, "onPause method runs!!!");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        //执行onStop()方法时可以存储一些数据或者进行一些CPU密集操作.
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        if (!message.equals("")){
+            Log.i(TAG_MYAPP, message);
+        }
+        Log.i(TAG_MYAPP, "onStop method runs!!!!");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG_MYAPP, "onDestroy method runs!!!!!");
     }
 }
